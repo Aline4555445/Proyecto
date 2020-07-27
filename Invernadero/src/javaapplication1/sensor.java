b85278e231624acd875d59c5e7b6a5c02033ef4b
YessiGomez <YessiGomez@users.noreply.github.com>
2020-07-27 01:56:59 -0500

    clase sensor

---
+++ Invernadero/src/javaapplication1/sensor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class sensor {
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        String solicitud_url = "http://192.168.99.100/api-invernadero/public/api/sensor";
+        curl api = new curl(solicitud_url, "POST");
+        
+        JSONObject sensor = new JSONObject();
+        sensor.put("id_invernadero", 2);
+          
+        JSONObject obj = api.apicall(sensor);
+          
+        System.out.println(obj.get("mensaje"));
+ 
+    
+
+    }
+
+}
